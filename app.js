2017287ef6514c314d53846b4e1c90d01fdfadb8
Alexander Ose <alex@alexose.com>
2013-02-02 12:06:32 -0800

    over-engineering

--- app.js
+++ app.js
@@ -7,11 +7,8 @@ app.configure(function () {
     app.set('port', process.env.PORT || 3000);
     app.use(express.logger('dev'));  /* 'default', 'short', 'tiny', 'dev' */
     app.use(express.bodyParser());
-    app.use('/public', express.static(__dirname + '/public'));
-    app.use('/components', express.static(__dirname + '/components'));
+    app.use(express.static(path.join(__dirname, 'components'))); 
 });
-
-app.get('/', convert.getStarted);
 
 app.get('/robots.txt', function(req, res){
     res.send('User-agent: *');
---
+++ components/convert/main.js
@@ -0,0 +1,33 @@
+var AppRouter = Backbone.Router.extend({
+
+    routes: {
+        ""                  : "home",
+        "about"             : "about"
+    },
+
+    initialize: function () {
+        this.headerView = new HeaderView();
+        $('.header').html(this.headerView.el);
+    },
+
+    home: function (id) {
+        if (!this.homeView) {
+            this.homeView = new HomeView();
+        }
+        $('#content').html(this.homeView.el);
+        this.headerView.selectMenuItem('home-menu');
+    },
+    about: function () {
+        if (!this.aboutView) {
+            this.aboutView = new AboutView();
+        }
+        $('#content').html(this.aboutView.el);
+        this.headerView.selectMenuItem('about-menu');
+    }
+
+});
+
+utils.loadTemplate(['HomeView', 'HeaderView', 'AboutView'], function() {
+    app = new AppRouter();
+    Backbone.history.start();
+});
