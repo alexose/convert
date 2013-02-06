aba7fb2c457fbc08247fdd51bfb47bdc88014146
Alexander Ose <alex@alexose.com>
2013-02-05 19:48:00 -0800

    chunked file uploading works

--- app.js
+++ app.js
@@ -1,6 +1,9 @@
 var express = require('express'), 
     app = express(),
     path = require('path'),
+    fs = require('fs'),
+    exec = require('child_process').exec,
+    util = require('util'),
     convert = require('./routes/convert');
 
 app.configure(function () {
@@ -20,9 +23,80 @@ app.get('/robots.txt', function(req, res){
     
 io.sockets.on('connection', function (socket) {
     socket.emit('welcome', { message : 'Connected to Socket.' });
+    socket.emit('news', { message : 'Here is some news.' });
     socket.on('my other event', function (data) {
         console.log(data);
     });
+
+    // via http://net.tutsplus.com/tutorials/javascript-ajax/how-to-create-a-resumable-video-uploade-in-node-js/
+    var files = {};
+    socket.on('start', function (data) { 
+        var name = data['name'];
+        files[name] = { 
+           filesize : data.size,
+           data   : "",
+           downloaded : 0
+        }
+
+        var file = files[name];
+        var place = 0;
+
+        try {
+           var stat = fs.statSync('files/' + name);
+           if(stat.isFile()) {
+              file.downloaded = stat.size;
+              place = stat.size / 524288;
+           }
+        } catch(er){} 
+        
+        // New file!
+        fs.open("files/" + name, "a", 0755, function(err, fd){
+           if(err){
+              console.log(err);
+           }
+           else {
+              file.handler = fd; 
+              socket.emit('more', { place : place, percent : 0 });
+           }
+        });
+    });
+
+    socket.on('upload', function (data){
+        var name = data.name,
+            file = files[name];
+        
+        file.downloaded += data.data.length;
+        file.data += data.data;
+        
+        // If file is fully uploaded
+        if (file.downloaded == file.filesize){
+            console.log('done');
+            fs.write(file.handler, file.data, null, 'binary', function(err, writen){
+                socket.emit('finished', { 'place' : place, 'percent' :  percent});
+            });
+        }
+
+        // If the Data Buffer reaches 10MB
+        else if (file.data.length > 10485760){ 
+            fs.write(file.handler, file.data, null, 'binary', function(err, writen){
+                console.log('reset');
+                file.data = ""; // Reset the buffer
+
+                var place = file.downloaded / 524288,
+                    percent = (file.downloaded / file.filesize) * 100;
+
+                socket.emit('more', { 'place' : place, 'percent' :  percent});
+            });
+        }
+        else{
+            console.log('writin');
+            var place = file.downloaded / 524288,
+                percent = (file.downloaded / file.filesize) * 100;
+
+            socket.emit('more', { 'place' : place, 'percent' :  percent});
+        }
+     });
+
 });
 
 server.listen(3000);
--- components/convert/main.js
+++ components/convert/main.js
@@ -1,31 +1,96 @@
 $(document).ready(function(){
     console.log('here we go!');
+    
+    var o = {
+        name    : 'convert',
+        element : $('#convert-wrapper'),
+        host    : 'http://gibson.local'
+    }
 
-    var convert = new Convert();
+    var convert = new Convert(o);
 
+    convert.begin();
 });
 
 // Initialize
-Convert = function(){
-    this.socket = io.connect('http://localhost');
+Convert = function(o){
+    this.o = o;
+    this.element = o.element;
+
+    this.element = $('<div />')
+        .attr('id', o.name + '-main')
+        .appendTo(o.element);
+
+    this.socket = socket = io.connect(o.host);
     
-    this.socket.on('news', function (data) {
+    socket.on('news', function (data) {
         console.log(data);
         socket.emit('my other event', { my: 'data' });
     });
+};
 
 
-}
+// Begin upload process
+Convert.prototype.begin = function(){
 
-Convert.Uploader = {
-    $(function () {
-        $('#fileupload').fileupload({
-            dataType: 'json',
-            done: function (e, data) {
-                $.each(data.result.files, function (index, file) {
-                    $('<p/>').text(file.name).appendTo(document.body);
-                });
+    // Append uploader to screen
+    var tmpl = ''+
+            '<form>' +
+            '<fieldset>' +
+            '<div id="{{name}}-uploader">' +
+                '<label for="{{name}}-filebox"> Choose A File: </label>' +
+                '<input type="file" id="{{name}}-filebox"><br>' +
+                '<label for="{{name}}-namebox"> Name: </label>' +
+                '<input type="text" id="{{name}}-namebox"><br>' +
+                '<button id="{{name}}-submit" type="submit" class="btn btn=primary">Upload!</button>' +
+            '</div>' +
+            '</fieldset>' +
+            '</form>' +         
+            '<div id="{{name}}-progress"><div id="{{name}}-progressbar"></div></div><span id="{{name}}-percent">0%</span>' +
+            '<span id="{{name}}-uploaded"> - <span id="{{name}}-total">0</span></span>';
+
+    var html = (Handlebars.compile(tmpl))({ name : this.o.name })
+    
+    var element = $(html).appendTo(this.element);
+
+    element.find('#' + this.o.name + '-submit')
+        .click($.proxy(function(evt){
+            evt.preventDefault();
+
+            var name = $('#' + this.o.name + '-namebox').val();
+
+            if (name !== "" && this.file){
+                this.reader = reader = new FileReader();
+
+                this.file.name = name;
+                reader.onload = function(e){
+                   socket.emit('upload', { 'name' : name, data : e.target.result });
+                }
+                socket.emit('start', { 'name' : name, 'size' : this.file.size });
             }
-        });
-    }); 
+        }, this));
+
+    element.find('#' + this.o.name + '-filebox')
+        .on('change', $.proxy(function(evt){
+            evt.preventDefault();
+            this.file = evt.target.files[0];
+        }, this));
+
+
+    this.socket.on('more', $.proxy(function(data){
+        console.log(data);
+
+        var place = data['place'] * 524288, // Next starting position 
+            min = Math.min(524288, (this.file.size - place)),
+            newFile; // The next block of data
+
+        // We'd better be webkit or mozilla!
+        newFile = this.file.slice(place, place + min); 
+ 
+        this.reader.readAsBinaryString(newFile);
+    }, this));
+
+    this.socket.on('finished', $.proxy(function(data){
+        console.log('all done.');
+    }, this));
 }
